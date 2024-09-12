0f6dc85c15be7461e700fc70666956bd5894a165
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-09-11 18:30:41 -0700

    Initialized all of the starting project files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Goal:  Create a applications that can pull recipes from an online API based on the user's input, and provide an option for the user to save certain recipes to their "recipe book",
+// saved in localStorage.
+
+function App() {
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
