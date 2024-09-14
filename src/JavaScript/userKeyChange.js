32fcfbf58a73d008350793218335d47b61974c58
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-09-14 10:01:09 -0700

    Quickly removed some unneeded imports

--- src/JavaScript/userKeyChange.js
+++ src/JavaScript/userKeyChange.js
@@ -1,6 +1,6 @@
 // Instead of saving my api key here or anywhere at all, instead I am requiring the user to paste in their own API key if they wish to use this app.
 // In the case where one does not want to have to do that every time, you can paste and store the key in this file instead but note that this file is not ignored by default for github.
-export let apiKey = '';
+export let apiKey = 'empty';
 
 export function changeKey(key) {
   apiKey = key;
