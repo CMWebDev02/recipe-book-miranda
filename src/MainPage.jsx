46f0cf946baac3478800348c9b140b24fadf2fb1
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-10-04 20:13:24 -0700

    Updated todo list

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -11,22 +11,22 @@ import { MealPlanner } from "./Pages/MealPlanner";
 // saved in localStorage.
 
 // TODO: 
-// Add a page that will generate random recipe ideas or show pictures of random dishes to make use of Promise.any()
-//  Have it be a race case since that is the main purpose or use of Promise.any()
-// - Add worker to take list of ingredients from the various recipes and convert them to an array of items.
+// - Have webWorker gather all nutrient totals from nutritional info and add them together.
+// Add a component to the home page that will generate random recipe ideas or show pictures of random dishes to make use of Promise.any()
+//  // Have it be a race case since that is the main purpose or use of Promise.any()
 // - Have webworker create a shopping list of all collected ingredients and have them return an array of shoppingList Items.
 // - Utilize await and async or promises to implement the various web workers when they are to gather nutritional info for all items and have them add it up to a total,
 //    to utilize the workers the way they are intended, making calculations.
-// - Utilize promise.all or promise.any to get nutritional info for all ingredients in a recipe using the food-data-central-api
 // - Create Home Page
 // - Add heading to display if no recipes are saved locally.
 
 // Requirements
 // Please write functions that contain the following syntax that you learned in the MDN Asynchronous JavaScript Tutorial assignments, note you can have a function that contains multiple syntaxes that are required
-// - Use of Promise.all() and Promise.any()
+// - Use of Promise.any()
 // - Use of async and await keywords
 // - A function that uses web-workers
 
+// // - Use of Promise.all() 
 // // - Use of the .json() method
 // // - Use of .then() method
 // // - A function that is using promises and interacting with promises
